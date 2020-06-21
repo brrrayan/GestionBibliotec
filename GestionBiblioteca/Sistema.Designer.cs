181010ffdda4abd9c769c7b6f502bb9792f7ce98
Brayan Nicolas <34951540+brrrayan@users.noreply.github.com>
2020-06-20 21:10:41 -0400

    Agregue mas MenuStrip a la vista principal

--- GestionBiblioteca/Sistema.Designer.cs
+++ GestionBiblioteca/Sistema.Designer.cs
@@ -30,14 +30,25 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.MenuStripPrincipal = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearNuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mantenimientoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearCategoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.consultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.integrantesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.crearNuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.crearCategoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bibliografiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cienciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idiomaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.libroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prestamoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.devolucionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
             this.MenuStripPrincipal.SuspendLayout();
             this.SuspendLayout();
@@ -56,14 +67,38 @@
             // 
             this.MenuStripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.archivoToolStripMenuItem,
-            this.mantenimientoToolStripMenuItem,
             this.consultaToolStripMenuItem,
+            this.mantenimientoToolStripMenuItem,
+            this.procesoToolStripMenuItem,
             this.ayudaToolStripMenuItem});
             this.MenuStripPrincipal.Location = new System.Drawing.Point(0, 0);
             this.MenuStripPrincipal.Name = "MenuStripPrincipal";
             this.MenuStripPrincipal.Size = new System.Drawing.Size(798, 24);
             this.MenuStripPrincipal.TabIndex = 0;
             this.MenuStripPrincipal.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearNuevoToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // crearNuevoToolStripMenuItem
+            // 
+            this.crearNuevoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bibliografiaToolStripMenuItem,
+            this.editoraToolStripMenuItem,
+            this.cienciaToolStripMenuItem,
+            this.idiomaToolStripMenuItem,
+            this.autoresToolStripMenuItem,
+            this.usuarioToolStripMenuItem,
+            this.libroToolStripMenuItem,
+            this.empleadoToolStripMenuItem});
+            this.crearNuevoToolStripMenuItem.Name = "crearNuevoToolStripMenuItem";
+            this.crearNuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.crearNuevoToolStripMenuItem.Text = "Crear nuevo";
             // 
             // mantenimientoToolStripMenuItem
             // 
@@ -72,6 +107,12 @@
             this.mantenimientoToolStripMenuItem.Name = "mantenimientoToolStripMenuItem";
             this.mantenimientoToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
             this.mantenimientoToolStripMenuItem.Text = "Mantenimiento";
+            // 
+            // crearCategoriaToolStripMenuItem
+            // 
+            this.crearCategoriaToolStripMenuItem.Name = "crearCategoriaToolStripMenuItem";
+            this.crearCategoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.crearCategoriaToolStripMenuItem.Text = "Crear Categoria";
             // 
             // consultaToolStripMenuItem
             // 
@@ -104,25 +145,74 @@
             this.panel2.Size = new System.Drawing.Size(800, 424);
             this.panel2.TabIndex = 1;
             // 
-            // archivoToolStripMenuItem
+            // bibliografiaToolStripMenuItem
             // 
-            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.crearNuevoToolStripMenuItem});
-            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.archivoToolStripMenuItem.Text = "Archivo";
+            this.bibliografiaToolStripMenuItem.Name = "bibliografiaToolStripMenuItem";
+            this.bibliografiaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bibliografiaToolStripMenuItem.Text = "Bibliografia";
             // 
-            // crearNuevoToolStripMenuItem
+            // editoraToolStripMenuItem
             // 
-            this.crearNuevoToolStripMenuItem.Name = "crearNuevoToolStripMenuItem";
-            this.crearNuevoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.crearNuevoToolStripMenuItem.Text = "Crear nuevo";
+            this.editoraToolStripMenuItem.Name = "editoraToolStripMenuItem";
+            this.editoraToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editoraToolStripMenuItem.Text = "Editora";
             // 
-            // crearCategoriaToolStripMenuItem
+            // cienciaToolStripMenuItem
             // 
-            this.crearCategoriaToolStripMenuItem.Name = "crearCategoriaToolStripMenuItem";
-            this.crearCategoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.crearCategoriaToolStripMenuItem.Text = "Crear Categoria";
+            this.cienciaToolStripMenuItem.Name = "cienciaToolStripMenuItem";
+            this.cienciaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cienciaToolStripMenuItem.Text = "Ciencia";
+            // 
+            // idiomaToolStripMenuItem
+            // 
+            this.idiomaToolStripMenuItem.Name = "idiomaToolStripMenuItem";
+            this.idiomaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.idiomaToolStripMenuItem.Text = "Idioma";
+            // 
+            // autoresToolStripMenuItem
+            // 
+            this.autoresToolStripMenuItem.Name = "autoresToolStripMenuItem";
+            this.autoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.autoresToolStripMenuItem.Text = "Autor";
+            // 
+            // usuarioToolStripMenuItem
+            // 
+            this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.usuarioToolStripMenuItem.Text = "Usuario";
+            // 
+            // libroToolStripMenuItem
+            // 
+            this.libroToolStripMenuItem.Name = "libroToolStripMenuItem";
+            this.libroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.libroToolStripMenuItem.Text = "Libro";
+            // 
+            // empleadoToolStripMenuItem
+            // 
+            this.empleadoToolStripMenuItem.Name = "empleadoToolStripMenuItem";
+            this.empleadoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.empleadoToolStripMenuItem.Text = "Empleado";
+            // 
+            // procesoToolStripMenuItem
+            // 
+            this.procesoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prestamoToolStripMenuItem,
+            this.devolucionToolStripMenuItem});
+            this.procesoToolStripMenuItem.Name = "procesoToolStripMenuItem";
+            this.procesoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.procesoToolStripMenuItem.Text = "Proceso";
+            // 
+            // prestamoToolStripMenuItem
+            // 
+            this.prestamoToolStripMenuItem.Name = "prestamoToolStripMenuItem";
+            this.prestamoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.prestamoToolStripMenuItem.Text = "Prestamo";
+            // 
+            // devolucionToolStripMenuItem
+            // 
+            this.devolucionToolStripMenuItem.Name = "devolucionToolStripMenuItem";
+            this.devolucionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.devolucionToolStripMenuItem.Text = "Devolucion";
             // 
             // FormMain
             // 
@@ -154,6 +244,17 @@
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem crearNuevoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem crearCategoriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bibliografiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editoraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cienciaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idiomaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem libroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prestamoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem devolucionToolStripMenuItem;
     }
 }
 
